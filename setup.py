1712a224a289d0806446579e3487239c2d81bc3d
Justin Vrana <justin.vrana@gmail.com>
2019-08-06 16:51:05 -0700

    updated installation tools
    * poetry
    * keats
    * pre-commit with black formatting

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(name='primer3plus',
-      packages=['primer3plus'],
-      package_data={
-          'primer3plus': ['primer3_params_raw.txt']
-      },
-      version='0.0.1'
-     )
