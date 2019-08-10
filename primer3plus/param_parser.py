232fb4f8a2de16d42dcbd225c0a34fa489942bd4
jvrana <justin.vrana@gmail.com>
2019-08-10 12:00:51 -0700

    init refactoring parameters

---
+++ primer3plus/param_parser.py
@@ -0,0 +1,248 @@
+import os
+import re
+from copy import deepcopy, copy
+from collections import MutableMapping
+
+# TODO: move default file to 'data'
+
+
+class ParamTypes(object):
+    GLOBAL = "GLOBAL"
+    PROGRAM = "PROGRAM"
+    OTHER = "OTHER"
+    SEQUENCE = "SEQUENCE"
+    TYPE = "PTYPE"
+
+
+class ParameterType(object):
+
+    def __init__(self, name, description, type, default):
+        self.name = name
+        self.description = description
+        self.type = type
+        self.default = default
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return "<{cls} {name} {type} default={default}>".format(
+            cls=self.__class__.__name__,
+            name=self.name,
+            type=self.type,
+            default=self.default
+        )
+
+class Parameter(ParameterType):
+
+    def __init__(self, ptype, value=None):
+        self.ptype = ptype
+        self._value = None
+        if value is None:
+            self.value = self.ptype.default
+        else:
+            self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, v):
+        if not issubclass(self.ptype.type, type(v)):
+            raise TypeError("Paramater {ptype} must be of type {type}".format(ptype=self.ptype, type=self.ptype.type))
+        self._value = self.ptype.type(v)
+
+    @property
+    def name(self):
+        return self.ptype.name
+
+    def set_default(self):
+        self.value = self.ptype.default
+
+    def copy(self):
+        p = self.__class__(self.ptype, deepcopy(self.value))
+        return p
+
+    def __str__(self):
+        return "<{cls} {name} {type} value={value} default={default}>".format(
+            cls=self.__class__.__name__,
+            name=self.name,
+            type=self.ptype.type,
+            value=self.value,
+            default=self.ptype.default
+        )
+
+    def __repr__(self):
+        return str(self)
+
+
+class Params(MutableMapping):
+
+    POST_LOAD_DEFAULTS = {"PRIMER_EXPLAIN_FLAG": 1}
+
+    def __init__(self):
+        self.params = {}
+
+    def update(self, data_dict):
+        for k, v in data_dict.items():
+            self[k] = v
+
+    def _post_load(self) -> dict:
+        self.update(self.POST_LOAD_DEFAULTS)
+
+
+    def _load(self, param_dict):
+        for k, v in param_dict.items():
+            ptype = ParameterType(
+                name=v['name'],
+                type=v['type'],
+                default=v['default'],
+                description=v['description']
+            )
+            p = Parameter(ptype, ptype.default)
+            self.params[p.name] = p
+        self._post_load()
+
+    def __setitem__(self, key, value):
+        self.params[key].value = value
+
+    def __getitem__(self, key):
+        return self.params[key].value
+
+    def __delitem__(self, key):
+        self.params[key].set_default()
+
+    def __len__(self):
+        return len(self.params)
+
+    def __iter__(self):
+        for k, v in self.params.items():
+            yield k
+
+    def set_defaults(self):
+        for v in self.params.values():
+            v.set_default()
+
+    def values(self):
+        for v in self.params.values():
+            yield v.value
+
+    def items(self):
+        for k, v in self.params.items():
+            yield k, v.value
+
+    def copy(self):
+        copied = Params()
+        for k, v in self.params.items():
+            copied.params[k] = v.copy()
+        return copied
+
+class ParamParser(object):
+    """
+    Reads the Primer3 documentation and creates the appropriate parameters.
+    """
+    default_file_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "primer3_params_raw.txt"
+    )
+
+    @staticmethod
+    def _parse_primer3_docs(docstr):
+        """
+        Parse the docs of the primer3 website
+        (https://htmlpreview.github.io/?https://github.com/libnano/primer3-py/master/primer3/src
+        /libprimer3/primer3_manual.htm#globalTags)
+
+        :param docstr: doc string
+        :type docstr: basestring
+        :return: params
+        :rtype: dict
+        """
+
+        params = {}
+        pattern = "(?P<name>\w+)\s+\((?P<type>[\w\s\"]+)\;\s+default\s+(?P<default>.+)\)\n(?P<description>.+)\n\n"
+
+        type_dict = {
+            "size range list": list,
+            "string": str,
+            "interval list": list,
+            "nucleotide sequence": str,
+            "int": int,
+            "space separated integers": list,
+            "float": float,
+            "ambiguous nucleotide sequence": str,
+            "boolean": bool,
+            "semicolon separated list of integer \"quadruples\"": list,
+            "semicolon separated list of integer quadruples": list
+        }
+
+        for m in re.finditer(pattern, docstr):
+            data = m.groupdict()
+            data["type_raw"] = data["type"]
+            data["type"] = type_dict[data["type"]]
+
+            default = data["default"]
+            data["default_raw"] = default
+            if data["type"] is str:
+                if default == "empty":
+                    default = ""
+                default = str(default)
+            elif data["type"] is list:
+                if default == "empty":
+                    default = []
+                elif re.match("(\d+)-(\d+)", default):
+                    list_match = re.match("(\d+)-(\d+)", default)
+                    default = [int(list_match.group(1)), int(list_match.group(2))]
+                default = list(default)
+            elif data["type"] is bool:
+                default = bool(int(default))
+            else:
+                default = data["type"](default)
+
+            data["default"] = default
+            data["value"] = default
+            name = data["name"]
+            params[name] = data
+
+        return params
+
+    @staticmethod
+    def _clean_dictionary(params: dict) -> dict:
+        """
+        Removes empty lists and empty strings from params
+        :return:
+        :rtype:
+        """
+        cleaned = dict(params)
+        ignore = ["SEQUENCE_ID"]
+        for k in params:
+            if k not in ignore:
+                v = params[k]
+                if hasattr(v, "__len__") and len(v) == 0:
+                    cleaned.pop(k)
+        return cleaned
+
+    @classmethod
+    def _open_primer3_params(cls, filepath=None):
+        if filepath is None:
+            filepath = cls.default_file_path
+        with open(filepath, "r") as f:
+            params_txt = f.read()
+            params = cls._parse_primer3_docs(params_txt)
+
+        for k, v in params.items():
+            if k.startswith("SEQUENCE"):
+                param_type = ParamTypes.SEQUENCE
+            elif k.startswith("PRIMER"):
+                param_type = ParamTypes.GLOBAL
+            elif k.startswith("P3"):
+                param_type = ParamTypes.PROGRAM
+            else:
+                raise Exception("Parameter {} not recognized".format(k))
+            v[ParamTypes.TYPE] = param_type
+
+        return params
+
+    @classmethod
+    def open(cls, filepath=None):
+        return cls._open_primer3_params(filepath)
---
+++ tests/test_params/test_param_parser.py
@@ -0,0 +1,82 @@
+from primer3plus.param_parser import ParamParser, Params
+
+import pytest
+
+@pytest.fixture(scope='function')
+def params():
+    param_data = ParamParser.open()
+
+    params = Params()
+    params._load(param_data)
+    return params
+
+
+def test_open(params):
+    assert params
+
+
+def test_open_and_load():
+    param_data = ParamParser.open()
+
+    params = Params()
+    params._load(param_data)
+
+
+class TestIter(object):
+
+    def test_iter(self, params):
+        x = list(params)
+        assert len(x) == len(params)
+        print(x)
+
+
+    def test_items(self, params):
+        x = list(params.items())
+        assert len(x) == len(params)
+        print(x)
+
+
+    def test_value(self, params):
+        x = list(params.values())
+        assert len(x) == len(params)
+        print(x)
+
+
+class TestSettersAndGetters(object):
+
+    def test_set_value_raises_type_error(self, params):
+        with pytest.raises(TypeError):
+            params['SEQUENCE_TARGET'] = 7
+        params['SEQUENCE_TARGET'] = []
+
+
+    def test_set_value(self, params):
+        params['SEQUENCE_TARGET'] = [1,2,3,4]
+        assert params['SEQUENCE_TARGET'] == [1,2,3,4]
+
+
+    def test_del_value(self, params):
+        params['SEQUENCE_TARGET'] = [1,2,3,4]
+        assert params['SEQUENCE_TARGET'] == [1,2,3,4]
+        del params['SEQUENCE_TARGET']
+        assert params["SEQUENCE_TARGET"] == []
+
+    def test_set_default(self, params):
+        params['SEQUENCE_TARGET'] = [1,2,3,4]
+        assert params['SEQUENCE_TARGET'] == [1,2,3,4]
+        params.set_defaults()
+        assert params["SEQUENCE_TARGET"] == []
+
+class TestCopy(object):
+
+    def test_copy(self, params):
+
+        params_copy = params.copy()
+        params['SEQUENCE_TARGET'].append(4)
+        assert params['SEQUENCE_TARGET'] == [4]
+        assert not params_copy["SEQUENCE_TARGET"] == [4]
+
+        assert params_copy.params["SEQUENCE_TARGET"].ptype is params.params['SEQUENCE_TARGET'].ptype
+
+
+
